f0cd4d6272c5ef91a1d66b804e9566276ba11575
gazi04 <gazmendhalili2016@gmail.com>
2026-07-01 12:22:31 +0200

    :lock: Add BelongsToTenant scope to TenantSetting, removed tenant_id from fillable list

--- app/Models/TenantSetting.php
+++ app/Models/TenantSetting.php
@@ -5,10 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Stancl\Tenancy\Database\Concerns\BelongsToTenant;
 
-#[Fillable(['tenant_id', 'key', 'value'])]
+#[Fillable(['key', 'value'])]
 class TenantSetting extends Model
 {
+    use BelongsToTenant;
+
     /**
      * @return BelongsTo<Tenant, $this>
      */
